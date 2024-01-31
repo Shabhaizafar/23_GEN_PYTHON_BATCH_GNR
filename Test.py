ecb23cd744116cfa9f678d316eeb0796e9362a25
Shabhai Zafar <shabhaizafar@gmail.com>
2024-01-31 07:54:31 +0530

    practice

---
+++ Test.py
@@ -0,0 +1,22 @@
+# import math 
+
+# math.sq
+
+# import sys
+
+# x = sys.argv
+# print(f"The script is running with the arguments: {x}")
+# print(len(x))
+
+# import os
+
+# def Arr(*arg):
+#     pass
+
+# Arr(1,2,3)
+# import math
+# print(math.floor(3.4))
+# # import math
+# print(math.copysign(3, -1))
+
+# print(math.fabs(3.4))
