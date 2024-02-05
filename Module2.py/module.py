32d67cccd9f6e161128cd360698f2d2ef13f85dd
Shabhai Zafar <shabhaizafar@gmail.com>
2024-02-05 19:43:22 +0530

    Date Module

---
+++ Module2.py/module.py
@@ -0,0 +1,13 @@
+# Datetime Module 
+
+import datetime as dt
+
+data = dt.datetime.now()
+
+# data.date()
+
+# print("Data : ",data.date())
+# print("Data : ",data.time())
+# print("Data : ",data.timetz())
+
+data.
