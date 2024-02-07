20d2fb3410613f875678d3b1906d7be60c0da44a
Shabhai Zafar <shabhaizafar@gmail.com>
2024-02-07 07:55:27 +0530

    Date Module

--- Module2.py/module.py
+++ Module2.py/module.py
@@ -1,6 +1,6 @@
 # Datetime Module 
 
-# import datetime as dt
+from datetime import date, datetime
 
 # data = dt.datetime.now()
 
@@ -12,3 +12,16 @@
 
 # combine method in data
 # print(data.combine(dt.date(2024,11,1), data.time()))
+
+# x = dt.date.day
+# x = dt.date
+
+data  = datetime.now()
+print(data)
+
+
+# https://docs.python.org/3/library/datetime.html
+
+
+#1)  https://www.sanfoundry.com/python-questions-answers-datetime-module-1/
+#2)  https://www.sanfoundry.com/python-questions-answers-datetime-module-2/
